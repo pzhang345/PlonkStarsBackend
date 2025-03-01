25f3f0840c2e430aa696aa199e859f3534cb8172
Patrick Zhang <pzhang345@gmail.com>
2025-03-01 05:15:33 -0500

    more bugs with game

--- app/api/game/games/basegame.py
+++ app/api/game/games/basegame.py
@@ -90,15 +90,15 @@ def find_map(map):
     query = GameMap.query
     map_name = map.get("name")
     if map_name:
-        query.filter_by(name=map_name)
+        query = query.filter_by(name=map_name)
     
     map_id = map.get("id")
     if map_id:
-        query.filter_by(uuid=map_id)
+        query = query.filter_by(uuid=map_id)
     
     map_creator = map.get("creator")
     if map_creator:
-        query.filter_by(uuid=map_creator)
+        query = query.filter_by(uuid=map_creator)
     
     return query.first()
     
