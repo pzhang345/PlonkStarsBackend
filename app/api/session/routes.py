66b4578292408acb6ff215133d218aaaca3fd91d
Patrick Zhang <pzhang345@gmail.com>
2025-04-11 14:47:59 -0400

    fixed the info endpoint

--- app/api/session/routes.py
+++ app/api/session/routes.py
@@ -9,7 +9,7 @@ session_bp = Blueprint("session_bp", __name__)
 @login_required
 def get_session_info(user):
     data = request.args
-    session = Session.query.filter_by(user_id=data.get("id")).first_or_404("Session not found")
+    session = Session.query.filter_by(uuid=data.get("id")).first_or_404("Session not found")
     return jsonify({
         "map":session.map.to_json(),
         "user":session.host.to_json(),
