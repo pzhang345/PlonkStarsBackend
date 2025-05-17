1b07735e02aae8beee06106171aee98701bef84f
Patrick Zhang <pzhang345@gmail.com>
2025-05-17 00:40:23 -0400

    changed the placement of the relationship statement in the crates model

--- app/models/crates.py
+++ app/models/crates.py
@@ -11,6 +11,8 @@ class Crate(db.Model):
     image = db.Column(db.String(50))
     price = db.Column(db.Integer, nullable=False)
     total_weight = db.Column(db.Integer, nullable=False, default=0)
+    
+    items = db.relationship("CrateItem", backref="crate", cascade="all, delete", passive_deletes=True)
 
     def __str__(self):
         return self.name
@@ -22,5 +24,6 @@ class CrateItem(db.Model):
     crate_id = db.Column(db.Integer, db.ForeignKey("crates.id", ondelete="CASCADE"), nullable=False)
     tier = db.Column(db.Enum(Tier), nullable=False)
     weight = db.Column(db.Integer, nullable=False)
-
-    crate = db.relationship("Crate", backref="crate_items", cascade="all, delete", passive_deletes=True)
+    
+    def __str__(self):
+        return f"{self.crate.name} item ({self.tier.name})"
