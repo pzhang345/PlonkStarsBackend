42d3ba4ed8b353a080eb60efe7974823829ab8af
Patrick Zhang <pzhang345@gmail.com>
2025-08-16 02:44:28 -0400

    socketio should now work using rediss://

--- app/fsocket.py
+++ app/fsocket.py
@@ -1,4 +1,13 @@
 from flask_socketio import SocketIO
 from config import Config
 
-socketio = SocketIO(cors_allowed_origins="*", message_queue=Config.REDIS_URL)
+args = {}
+if Config.REDIS_URL.startswith("rediss://"):
+    import ssl
+    args["ssl_cert_reqs"] = ssl.CERT_NONE
+    
+socketio = SocketIO(
+    cors_allowed_origins="*",
+    message_queue=Config.REDIS_URL,
+    **args
+)
