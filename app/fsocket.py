cd83db2b85938030ed1b6761fe254bc6db9dad78
Patrick Zhang <pzhang345@gmail.com>
2025-08-16 22:31:45 -0400

    hopefully socketio with message queue works now

--- app/fsocket.py
+++ app/fsocket.py
@@ -1,7 +1,8 @@
 from flask_socketio import SocketIO
 from config import Config
-import ssl
-    
+
+redis_ssl_url = Config.REDIS_URL + "/0?ssl_cert_reqs=none" if Config.REDIS_URL.startswith("rediss://") else Config.REDIS_URL
 socketio = SocketIO(
     cors_allowed_origins="*",
+    message_queue=redis_ssl_url
 )
