58374ebd6a753f07210e753e3cf0395392b1dc39
Patrick Zhang <pzhang345@gmail.com>
2025-10-15 20:17:05 -0400

    hopeful this fixes beat

--- app/my_celery/base_celery.py
+++ app/my_celery/base_celery.py
@@ -5,7 +5,19 @@ from models.db import db
 
 redis_ssl_url = Config.REDIS_URL + "/0?ssl_cert_reqs=CERT_NONE" if Config.REDIS_URL.startswith("rediss://") else Config.REDIS_URL
 
-celery = Celery(__name__, broker=redis_ssl_url, backend=redis_ssl_url)
+broker_use_ssl_config = None
+if Config.REDIS_URL.startswith("rediss://"):
+    broker_use_ssl_config = {"ssl_cert_reqs": ssl.CERT_NONE}
+    
+celery = Celery(
+    __name__, 
+    broker=redis_ssl_url, 
+    backend=redis_ssl_url,
+    broker_use_ssl=broker_use_ssl_config
+)
+
+if broker_use_ssl_config:
+    celery.conf.broker_use_ssl = broker_use_ssl_config
 
 def init_celery(app):
     class ContextTask(celery.Task):
@@ -21,20 +33,17 @@ def init_celery(app):
         enable_utc=True
     )
     
-    from my_celery.daily_tasks import init_daily_tasks
-    init_daily_tasks(celery)
-    
-    if Config.REDIS_URL.startswith("rediss://"):
-        celery.conf.update(
-            broker_use_ssl = {"ssl_cert_reqs": ssl.CERT_NONE}
-        )
-    
+
     @signals.task_postrun.connect
     def cleanup_sessions(*args, **kwargs):
         with app.app_context():
             db.session.remove()
     
     celery.autodiscover_tasks(['api.game.tasks'])
+    
+    from my_celery.daily_tasks import init_daily_tasks
+    init_daily_tasks(celery)
+    
     return celery
 
 
